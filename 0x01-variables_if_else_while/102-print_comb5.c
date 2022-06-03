0acb46da6385f45afbefcae6beded7c375fa10fc
Oluwaseun Oyebajo <ohluwacheunjava@gmail.com>
2022-06-03 16:43:47 -0700

    software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,24 +11,22 @@ int main(void)
 	
 	for (i = 0; i <= 99; i++)
 	{
-		for (j = 0; j <= 99; j++)
+	for (j = 0; j <= 99; j++)
+	{
+	if (i < j && i != j)
+	{
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+		putchar(' ');
+		putchar((j / 10) + '0');
+		putchar((j % 10) + '0');
+		if (i != 98 || j != 99)
 		{
-			if (i < j && i != j)
-			{
-				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
-				putchar(' ');
-				putchar((j / 10) + '0');
-				putchar((j % 10) + '0');
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-					
-				}
-
-			}
+			putchar(',');
+			putchar(' ');					
 		}
+	}
+	}
 	}
 	putchar('\n');
 	return (0);
