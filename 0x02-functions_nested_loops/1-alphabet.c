d1da36706437d076e9caa9b9f573f49fa221b539
Oluwaseun Oyebajo <ohluwacheunjava@gmail.com>
2022-06-07 15:07:10 -0700

    pu

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+
+	_putchar('\n);
+
+	return (0);
+}
