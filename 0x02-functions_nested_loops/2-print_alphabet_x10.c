3a110018a765c67dedbf651720f0b0b98a33491c
Oluwaseun Oyebajo <ohluwacheunjava@gmail.com>
2022-06-07 16:34:10 -0700

    10xalphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; ++1)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar(c);
+	}
+	return (0);
+}
