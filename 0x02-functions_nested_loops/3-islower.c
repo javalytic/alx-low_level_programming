745994f8d941167dcf659addf44b33d983004016
Oluwaseun Oyebajo <ohluwacheunjava@gmail.com>
2022-06-08 03:39:24 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
  * _islower - Return 1 if letter is lowercase, 0 if not.
  * Return: 1 if lowercase, 0 if not lowercase
  */
-int_islower(int c)
+int _islower(int c)
 {
 	if (c > 96 && c < 123)
 	return (1);
