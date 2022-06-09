398d4aef6e325a47401d592e61121e6c1f5e854f
Oluwaseun Oyebajo <ohluwacheunjava@gmail.com>
2022-06-09 13:38:01 -0700

    numbers speak

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,15 @@
 #include "main.h"
+
 /**
- * print_numbers - print number from 0 to 9
+ * print_numbers - a function that prints the numbers, from 0 to 9
  *
- * Return: always 0
+ * Return: 0-9 followed by a new line
  */
 void print_numbers(void)
 {
-	int n;
+	int i;
 
-	for (n = 0; n < 10; n++)
-	{
-		_putchar(n + '0');
-	}
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
 	_putchar('\n');
 }
