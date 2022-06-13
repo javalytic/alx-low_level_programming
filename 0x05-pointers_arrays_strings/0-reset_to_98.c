16accd38184c931ad06991e3e24c32d3c49e1953
Oluwaseun Oyebajo <ohluwacheunjava@gmail.com>
2022-06-13 08:21:07 -0700

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset the value of 98
+ *
+ * @n: variable value is going to be 98
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
