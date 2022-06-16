fa25f343e1452603fdab3f7f1e0c04a61ae6f5da
Oluwaseun Oyebajo <ohluwacheunjava@gmail.com>
2022-06-16 07:56:51 -0700

    Numbers have life

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,7 +8,7 @@
 int base10(int n)
 {
 	int base = 10;
-	
+
 	while (n > 0)
 	{
 		base *= 10;
@@ -25,23 +25,23 @@ int base10(int n)
 void print_number(int n)
 {
 	int power;
-	
+
 	power = base10(8);
-	
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
 	}
-	
+
 	if (n == 0)
 		_putchar('0');
-	
+
 	else
 	{
 		while (n / power == 0)
 			power /= 10;
-		
+
 		while (power >= 1)
 		{
 			_putchar((n / power) + '0');
