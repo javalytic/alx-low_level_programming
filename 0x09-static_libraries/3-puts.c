632c3f242878bddda32403680fbaec6b3b2621ef
Oluwaseun Oyebajo <ohluwacheunjava@gmail.com>
2022-06-25 11:57:20 -0700

    static

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _puts - Write a function that prints a string.
- * followed by a new line, to stdout.
+ * _puts - Write a function that prints a string, followed
+ * by a new line, to stdout.
  * @str: an input string
  * Return: Nothing
  */
 void _puts(char *str)
 {
 	int i = 0;
-
+	
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
