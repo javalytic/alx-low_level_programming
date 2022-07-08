c25e8e52f6231349d79c1615a6cfab3a6983fc43
Oluwaseun Oyebajo <ohluwacheunjava@gmail.com>
2022-07-07 21:31:35 -0700

    fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,31 +1,38 @@
-#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "3-calc.h"
 
 /**
- * main - program to perform simple operations
- * @argc: argument count
- * @argv: array of arguments
- * Return: exits 0 normally, 98 on argc error, 99 on illegal operator
+ * main - calculator
+ * @argc: argument counter
+ * @argv: argument vector or pointer array
+ * Return: 0 or Error
  */
 int main(int argc, char *argv[])
 {
-	int (*func)(int, int);
+	int a, b, r;
+	int (*calc)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	func = get_op_func(argv[2]);
-
-	if (func == NULL)
+	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	calc = get_op_func(argv[2]);
+	if (calc == NULL)
+	{
+		exit(99);
+	}
+	r = calc(a, b);
+	printf("%d\n", r);
+
 	return (0);
 }
